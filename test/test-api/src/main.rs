2a4cfefdfc76e699d089471ec2266539ef4c969c
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2025-03-26 19:18:09 +0900

    test/test-api: Fixed BUG

--- test/test-api/src/main.rs
+++ test/test-api/src/main.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 use dag_bpf::send_dag_task_to_bpf;
 use dag_task::dag::TaskGraphBuilder;
 use bpf_comm::urb::UserRingBuffer;
@@ -10,12 +12,12 @@ fn main() {
 
 	let mut builder = TaskGraphBuilder::new();
 
-	builder.reg_reactor(0, vec!["topic2".to_string()], vec![]);
-	builder.reg_reactor(1, vec!["topic0".to_string()], vec!["topic1".to_string()]);
-	builder.reg_reactor(2, vec![], vec!["topic0".to_string()]);
-	builder.reg_reactor(3, vec![], vec!["topic3".to_string()]);
-	builder.reg_reactor(4, vec!["topic0".to_string(), "topic1".to_string()], vec![]);
-	builder.reg_reactor(5, vec!["topic3".to_string()], vec!["topic2".to_string()]);
+	builder.reg_reactor(0, vec![Cow::from("topic2")], vec![]);
+	builder.reg_reactor(1, vec![Cow::from("topic0")], vec![Cow::from("topic1")]);
+	builder.reg_reactor(2, vec![], vec![Cow::from("topic0")]);
+	builder.reg_reactor(3, vec![], vec![Cow::from("topic3")]);
+	builder.reg_reactor(4, vec![Cow::from("topic0"), Cow::from("topic1")], vec![]);
+	builder.reg_reactor(5, vec![Cow::from("topic3")], vec![Cow::from("topic2")]);
 
 	let task_graph = builder.build();
 	let dag_tasks = task_graph.to_dag_tasks().unwrap();
