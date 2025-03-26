becbdb73b238c4fd3b73a096f247e794299b86d1
Takumi Jin <primenumber_2_3_5@yahoo.co.jp>
2025-03-26 17:44:07 +0900

    WIP: Add reactor-api library

---
+++ lib/reactor-api/src/lib.rs
@@ -0,0 +1,147 @@
+use std::error::Error;
+use std::sync::mpsc::Receiver;
+use std::sync::mpsc::Sender;
+use std::thread::sleep;
+use std::thread::JoinHandle;
+
+use std::collections::HashMap;
+use std::borrow::Cow;
+use std::sync::LazyLock;
+use std::sync::Mutex;
+use std::time::Duration;
+use std::vec;
+
+type Tid = u32;
+type Subscriber = (Tid, Sender<MsgItem>);
+
+struct Topic {
+	subscribers: Vec<Subscriber>,
+}
+
+struct TopicManager {
+	topics: HashMap<Cow<'static, str>, Topic>,
+}
+
+static TOPIC_MANAGER: LazyLock<Mutex<TopicManager>> = LazyLock::new(|| {
+	Mutex::new(TopicManager { topics: HashMap::new() })
+});
+
+impl TopicManager {
+	fn subscribe(&mut self, topic_name: Cow<'static, str>, sender: Sender<MsgItem>)
+	{
+		println!("subscribe: {}, sender={sender:?}", topic_name);
+
+		if let Some(topic) = self.topics.get_mut(&topic_name) {
+			topic.subscribers.push((1729, sender));
+		} else {
+			let subscribers = vec![(1729, sender)];
+			let topic = Topic {
+				subscribers
+			};
+			self.topics.insert(topic_name, topic);
+		}
+	}
+
+	fn publish(&mut self, topic_name: Cow<'static, str>, msg: MsgItem)
+	{
+		println!("[pub] publish to {}: msg={:?}", topic_name, msg);
+
+		if let Some(topic) = self.topics.get_mut(&topic_name) {
+			for (tid, subscriber) in &mut topic.subscribers {
+				println!("[pub] publish to thread{tid}: msg={msg:?}");
+				subscriber.send(msg.clone()).unwrap();
+			}
+		} else {
+			// topicのみを作成する。
+			// subscribersは空リストとする。
+			let subscribers = vec![];
+			let topic = Topic {
+				subscribers
+			};
+			self.topics.insert(topic_name, topic);
+		}
+	}
+}
+
+#[derive(Debug, Clone)]
+pub enum MsgItem {
+	U32(u32),
+}
+
+fn register_subscription(subscribe_topic_names: &Vec<Cow<'static, str>>) -> Vec<Receiver<MsgItem>>
+{
+	let mut manager = TOPIC_MANAGER.lock().unwrap();
+
+	let mut rcvers = vec![];
+	for topic in subscribe_topic_names {
+		let (snder, rcver) = std::sync::mpsc::channel();
+		manager.subscribe(topic.clone(), snder);
+		rcvers.push(rcver);
+	}
+	rcvers
+}
+
+/// let f = |v: Vec<MsgItem>| -> Vec<MsgItem> { ... }
+/// spawn_reactor()
+pub fn spawn_reactor<F>(
+	f: F,
+	subscribe_topic_names: Vec<Cow<'static, str>>,
+	publish_topic_names: Vec<Cow<'static, str>>,
+) -> Result<JoinHandle<()>, Box<dyn Error>>
+where
+	F: Fn(Vec<MsgItem>) -> Vec<MsgItem> + Send + 'static
+{
+
+	let handle = std::thread::spawn(move || {
+		println!("Thread is spawned!");
+
+		// channelを作成する
+		let rcvrs = register_subscription(&subscribe_topic_names);
+
+		loop {
+			let mut args = vec![];
+			for rcvr in &rcvrs {
+				let msg = rcvr.recv().unwrap();
+				args.push(msg);
+			}
+
+			let mut ret = f(args);
+			for topic in publish_topic_names.iter().rev() {
+				let mut manager = TOPIC_MANAGER.lock().unwrap();
+				manager.publish(topic.clone(), ret.pop().unwrap());
+			}
+		}
+	});
+
+	println!("Thread {:?} is spawned", handle.thread().id());
+
+	Ok(handle)
+} 
+
+/// let f = || -> Vec<MsgItem> { ... }
+/// spawn_reactor()
+pub fn spawn_periodic_reactor<F>(
+	f: F,
+	publish_topic_names: Vec<Cow<'static, str>>,
+	period: Duration,
+) -> Result<JoinHandle<()>, Box<dyn Error>>
+where
+	F: Fn() -> Vec<MsgItem> + Send + 'static
+{
+	let handle = std::thread::spawn(move || {
+		println!("Thread is spawned!");
+		loop {
+			sleep(period);
+
+			let mut ret = f();
+			for topic in publish_topic_names.iter().rev() {
+				let mut manager = TOPIC_MANAGER.lock().unwrap();
+				manager.publish(topic.clone(), ret.pop().unwrap());
+			}
+		}
+	});
+
+	println!("Thread {:?} is spawned", handle.thread().id());
+
+	Ok(handle)
+} 
---
+++ lib/reactor-api/src/main.rs
@@ -0,0 +1,63 @@
+use std::time::Duration;
+use std::borrow::Cow;
+
+use reactor_api::*;
+
+const BUSY_UNIT: usize = 50000;
+
+fn busy(weight: usize)
+{
+	for i in 0..weight {
+		std::hint::black_box(i);
+	}
+}
+
+fn main()
+{
+	let mut handles = vec![];
+
+	/* src node */
+	let f = || {
+		println!("\n[*] periodic src node!");
+		busy(1000 * BUSY_UNIT);
+		let ret = vec![MsgItem::U32(1729)];
+		ret
+	};
+	let src = spawn_periodic_reactor(
+		f,
+		vec![Cow::from("topic0")],
+		Duration::from_secs(1),
+	).unwrap();
+	handles.push(src);
+
+	/* second node */
+	let f = |v| {
+		println!("second node! arg={:?}", v);
+		busy(1000 * BUSY_UNIT);
+		v
+	};
+	let handle = spawn_reactor(
+		f,
+		vec![Cow::from("topic0")],
+		vec![Cow::from("topic1")],
+	).unwrap();
+	handles.push(handle);
+
+	/* third node */
+	let f = |v| {
+		println!("final node! arg={:?}", v);
+		busy(1000 * BUSY_UNIT);
+		vec![]
+	};
+	let handle = spawn_reactor(
+		f,
+		vec![Cow::from("topic1")],
+		vec![],
+	).unwrap();
+	handles.push(handle);
+
+	/* join */
+	for handle in handles {
+		handle.join().unwrap();
+	}
+}
